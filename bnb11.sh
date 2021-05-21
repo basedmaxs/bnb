92b1d5fc0438d164c9610c7aa4cf32d017162bd7
basedmaxs <83960316+basedmaxs@users.noreply.github.com>
2021-05-21 17:43:48 +0700

    Create bnb11.sh

---
+++ bnb11.sh
@@ -0,0 +1,6 @@
+# bnb
+sudo apt update
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.24/lolMiner_v1.24a_Lin64.tar.gz
+tar xf lolMiner_v1.24a_Lin64.tar.gz
+cd 1.24a
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BNB:bnb136ns6lfw4zs5hg4n85vdthaad7hq5m4gtkgf23:106863498.wk11
